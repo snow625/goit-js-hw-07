86c8eaee6c1767a904b0ff067f83cd135a57a264
Dmitriy Narishkin <97532271+snow625@users.noreply.github.com>
2022-05-22 19:57:56 +0300

    start files

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galeryPlace = document.querySelector(".gallery");
+galeryPlace.addEventListener("click", onClickGalaryItem);
+
+addGaleryToHTML(galleryItems);
+
+function createOneItemEl({ preview, original, description } = {}) {
+  return `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`;
+}
+
+function renderGaleryItems(arrOfItems) {
+  return arrOfItems.map((el) => createOneItemEl(el)).join("");
+}
+
+function addGaleryToHTML(galleryItems) {
+  galeryPlace.insertAdjacentHTML("beforeend", renderGaleryItems(galleryItems));
+}
+
+function onClickGalaryItem(event) {
+  if (event.target.nodeName === "IMG") {
+    event.preventDefault();
+    const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`);
+
+    instance.show();
+    window.addEventListener("keydown", closeModal);
+    function closeModal(event) {
+      if (event.code === "Escape") {
+        instance.close();
+        window.removeEventListener("keydown", closeModal);
+      }
+    }
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galeryPlace = document.querySelector(".gallery");
+
+addGaleryToHTML(galleryItems);
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  /* options */
+});
+
+function createOneItemEl({ preview, original, description } = {}) {
+  return `<div class="gallery__item">
+    <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+  </div>`;
+}
+
+function renderGaleryItems(arrOfItems) {
+  return arrOfItems.map((el) => createOneItemEl(el)).join("");
+}
+
+function addGaleryToHTML(galleryItems) {
+  galeryPlace.insertAdjacentHTML("beforeend", renderGaleryItems(galleryItems));
+}
